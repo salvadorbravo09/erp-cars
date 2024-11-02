48fa56eb1fed64689fcc6224c41b7aa1a4f02e98
Salvador Bravo Carrillo <sa.bravo0901@gmail.com>
2024-11-02 17:27:16 -0300

    first commit

---
+++ src/main/java/com/sbravoc/erp/cars/ErpCarsApplication.java
@@ -0,0 +1,13 @@
+package com.sbravoc.erp.cars;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErpCarsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ErpCarsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sbravoc/erp/cars/ErpCarsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sbravoc.erp.cars;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ErpCarsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
