e5550d1026ffd50441c2f5759193c507ebec15aa
Salvador Bravo Carrillo <sa.bravo0901@gmail.com>
2024-11-02 18:56:51 -0300

    add anotation for the repository

--- src/main/java/com/sbravoc/erp/cars/repositories/CarRepository.java
+++ src/main/java/com/sbravoc/erp/cars/repositories/CarRepository.java
@@ -2,6 +2,8 @@ package com.sbravoc.erp.cars.repositories;
 
 import com.sbravoc.erp.cars.entities.models.Car;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CarRepository extends JpaRepository<Car, Long> {
 }
