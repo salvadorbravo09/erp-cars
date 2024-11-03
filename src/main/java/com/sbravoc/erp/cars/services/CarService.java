7169b7bf2d008fb9df2c37418577940273dbfe79
Salvador Bravo Carrillo <sa.bravo0901@gmail.com>
2024-11-03 16:04:00 -0300

    add optional in getCarById

--- src/main/java/com/sbravoc/erp/cars/services/CarService.java
+++ src/main/java/com/sbravoc/erp/cars/services/CarService.java
@@ -3,12 +3,13 @@ package com.sbravoc.erp.cars.services;
 import com.sbravoc.erp.cars.entities.models.Car;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface CarService {
 
     List<Car> getAllCars();
 
-    Car getCarById(Long id);
+    Optional<Car> getCarById(Long id);
 
     void saveCar(Car car);
 
