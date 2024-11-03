10d0755f832c487fdbd560e6343ec45867bad6d0
Salvador Bravo Carrillo <sa.bravo0901@gmail.com>
2024-11-03 17:02:43 -0300

    add basic documentation

--- src/main/java/com/sbravoc/erp/cars/controllers/CarController.java
+++ src/main/java/com/sbravoc/erp/cars/controllers/CarController.java
@@ -17,11 +17,13 @@ public class CarController {
 
     private final CarRepository carRepository;
 
+    // List all Cars
     @GetMapping
     public List<Car> getAllCars() {
         return carRepository.findAll();
     }
 
+    // List Car by ID
     @GetMapping("/{id}")
     public ResponseEntity<Car> getCarById(@PathVariable Long id) {
         Optional<Car> searchCar = carRepository.findById(id);
@@ -32,12 +34,14 @@ public class CarController {
         }
     }
 
+    // Create new Car
     @PostMapping
     public ResponseEntity<Car> saveCar(@RequestBody Car car) {
         Car newCar = carRepository.save(car);
         return ResponseEntity.status(HttpStatus.CREATED).body(newCar);
     }
 
+    // Delete car if exists
     @DeleteMapping("/{id}")
     public ResponseEntity<?> deleteCar(@PathVariable Long id) {
         if (carRepository.existsById(id)) {
