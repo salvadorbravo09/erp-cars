f38318bde4261611fde89880c606827a9367bfc2
Salvador Bravo Carrillo <sa.bravo0901@gmail.com>
2024-11-04 18:29:59 -0300

    add validation rules in entity

--- src/main/java/com/sbravoc/erp/cars/entities/models/Car.java
+++ src/main/java/com/sbravoc/erp/cars/entities/models/Car.java
@@ -1,6 +1,8 @@
 package com.sbravoc.erp.cars.entities.models;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -18,17 +20,22 @@ public class Car {
     private Long id;
 
     @Column(name = "make")
+    @NotBlank(message = "La marca no puede estar vacía")
     private String make;
 
     @Column(name = "model")
+    @NotBlank(message = "El modelo no puede estar vacío")
     private String model;
 
     @Column(name = "year")
+    @Min(value = 1886, message = "El año debe ser válido.")
     private int year;
 
-    @Column(name = "patent")
+    @Column(name = "patent", unique = true)
+    @NotBlank(message = "La patente no puede estar vacía")
     private String patent;
 
     @Column(name = "color")
+    @NotBlank(message = "El color no puede estar vacío")
     private String color;
 }
