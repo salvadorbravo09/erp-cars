5a1ac9efa4bd647ec34c05e1b793903d8033975f
Salvador Bravo Carrillo <sa.bravo0901@gmail.com>
2024-11-04 18:04:29 -0300

    add CORS

--- src/main/java/com/sbravoc/erp/cars/controllers/CarController.java
+++ src/main/java/com/sbravoc/erp/cars/controllers/CarController.java
@@ -14,6 +14,7 @@ import java.util.Optional;
 @RestController
 @RequestMapping("/api/v1/car")
 @RequiredArgsConstructor
+@CrossOrigin("http://localhost:4200/")
 public class CarController {
 
     private final CarService carService;
